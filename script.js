f647a23dacd14e9e07d3f01066fb268cbc0c74fb
Dreeyas <a.navarretegarrido@gmail.com>
2023-07-12 21:05:15 -0400

    funcionalidad del boton agregar tarea, cambio de input

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const date = document.querySelector("#date") //Variable para la fecha
 const enterTask = document.querySelector("#enter_task") //Variable para agregar una tarea desde el input
 const list = document.querySelector("#list") //Variable para manejar la lista de tareas
 const submitButton = document.querySelector("#submit") //Variable para manejar el boton de subida (+)
+const lineThrought = 'line-through';
 
 //Declaración de funciones
 function addTask (task) {
     const listTemplate = `
                     <li id= "listTemplate">
-                        <i class="bi bi-square" data="done" id="0"></i>
+                        <input type="checkbox">
                         <p class="task_text">${task}</p>
                         <i class="bi bi-pencil-square" data="edit" id="0"></i>
                         <i class="bi bi-trash-fill" data="delete" id="0"></i>
@@ -16,6 +17,45 @@ function addTask (task) {
     list.insertAdjacentHTML("beforeend", listTemplate)
 }
 
+//Aca definiré que pasa al hacer click en el botón
+submitButton.addEventListener('click', () => {
+    const task = enterTask.value
+    if (task) {
+        addTask(task)
+    } 
+        enterTask.value='' 
+});
+
+//Aca definiré que pasa al apretar enter
+enterTask.addEventListener('keydown', function (e) { 
+    if (e.key === 'Enter') {
+        const task = enterTask.value
+         if (task) {
+            addTask(task)
+        } 
+        enterTask.value=''
+        } 
+});
+
+//Definiendo que pasa si está hecha la tarea
 
 
 
+
+
+
+
+
+//Aca experimentando con borrar
+function completeTask (e) { 
+    let task = e.target.parentNode. parentNode;  
+    task.classList.toggle('completed'); 
+}
+
+function deleteTask (e) { 
+    let task = e.target.parentNode. parentNode; 
+    task.remove();
+}
+
+iconDone.addEventListener ('click', completeTask)
+iconDelete.addEventListener ('click', deleteTask)
